e2459e93e16aee2335c060726051da8f5ca2290f
Maciej Gierwatowski <maciej.gierwatowski@gmail.com>
2020-04-30 17:03:31 +0200

    mx_tools

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,18 @@
-output "rsd_master_password" {
+/*output "rsd_master_password" {
   description = "RDS master password decrypted from KMS"
   value       = aws_ssm_parameter.secret.value
+}*/
+
+
+output "value" {
+  description = "Stored value"
+  value       = join("", aws_ssm_parameter.parameter.*.value)
+}
+
+output "ssm_arn" {
+  value = aws_ssm_parameter.parameter.*.arn
+}
+
+output "kms_arn" {
+  value = var.enabled && var.kms_encrypt ? [module.kms_key.key_arn] : []
 }
