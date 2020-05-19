b75b2afc1996f3611343f8ae7e3d35d3c854e5e3
Maciej Gierwatowski <maciej.gierwatowski@gmail.com>
2020-05-19 14:34:42 +0200

    kms outputs

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+---
+# 0.1.1
+## Main changes
+    - output `kms_arn` depends only on `var.kms_encrypt`
+    - added output `kms_arn_single`, which returns string. Left `kms_arn` for backward compatibility.
+    - added output `kms_key_id`
+
+## Deprecation warning
+    - output `kms_arn` is not supported anymore
+
+
+# 0.1.0
+
+## Main changes
+* Start
--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,16 @@ output "ssm_arn" {
   value = aws_ssm_parameter.parameter.*.arn
 }
 
+# DEPRECATED:
 output "kms_arn" {
   value = var.enabled && var.kms_encrypt ? [module.kms_key.key_arn] : []
+  description = "DEPRECATED!"
+}
+
+output "kms_arn_single" {
+  value = var.kms_encrypt ? module.kms_key.key_arn : ""
+}
+
+output "kms_key_id" {
+  value = var.kms_encrypt ? module.kms_key.key_id : ""
 }
